31e5af50b5e91dccfb95e111bea26e92a55252b9
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-03-21 19:29:29 +0000

    adding import statement for aos css file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Hero from '../components/hero/hero';
 import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 AOS.init()
 
