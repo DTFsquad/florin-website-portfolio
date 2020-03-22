0ac5b54fbfcf73e9ebc76e40baa029aa34f3d58f
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-03-22 13:17:16 +0000

    build the index.js in config folder

---
+++ config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: `Florin Dumitru | FrontEnd Developer`,
+    siteUrl: 'https://www.florin-dumitru.co.uk',
+    description: `I am a highly motivated Front End Developer based in London, United Kingdom, passionate about creating rich user interfaces and interactive web applications using a wide range of JavaScript technologies and libraries.
+    I strongly believe in the importance of writing clean, modular and loosely coupled code that is easy to document, maintain and reuse.
+    I have experience working with HTML, CSS, JavaScript, React, TypeScript, GraphQL, MongoDB, Nuxt, Jest, Webpack, Node, Express.`,
+    keywords: `Florin Dumitru, github, DTFsquad,`,
+    author: `Florin Dumitru`,
+    siteLanguage: 'en_GB',
+    location: 'London. UK',
+    email: 'dumitruflorin19@yahoo.com',
+    github: 'https://github.com/DTFsquad',
+    twitter: '@flololo',
+    socialMedia: [
+        {
+            name: 'GitHub',
+            url: 'https://github.com/DTFsquad'
+        },
+        {
+            name: 'Linkedin',
+            url: 'https://www.linkedin.com/in/florin-dumitru-a997941a/'
+        },
+        {
+            name: 'Twitter',
+            url: 'https://twitter.com/flololo'
+        }
+    ]
+}
