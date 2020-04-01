50fafd06a8a2941893f17524bcad5872b2da3e97
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-01 11:13:49 +0100

    glabl style available in both states

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import { Global, css } from '@emotion/core';
 import globalStyles from '../styles/global';
 import Footer from './footer/footer';
@@ -7,27 +8,28 @@ import LoadingIntro from './loadingIntro/loadingIntro';
 import Header from './header/header';
 
 
-const main = css`
+const Main = styled.main`
   margin: 0 auto;
 `;
 
 const Layout = ({ children }) => {
   const [ loading, setLoading] = useState(true);
 
-  return !loading ? (
-    <>
-      <Header />
+  return (
+    <React.Fragment>
       <Global styles={globalStyles} />
-      <main css={main}>{children}</main>
-      <Footer />
-    </>
-  ) : (
-    <>
-      <Global styles={globalStyles} />
-      <LoadingIntro setLoading={setLoading}/>
-    </>
-  );
-};
+      {!loading ? (
+        <React.Fragment>
+          <Header />
+          <Main>{children}</Main>
+          <Footer />
+        </React.Fragment>
+      ) : (
+        <LoadingIntro setLoading={setLoading} />
+      )}
+    </React.Fragment>
+    );
+  };
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
