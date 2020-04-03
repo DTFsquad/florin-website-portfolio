08710906c30d2143e8009d85f6bd5858d324e7a9
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-03 22:02:41 +0100

    add aos in lifecycle 2 avoid server side error document not found

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
-
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom';
+import 'aos/dist/aos.css';
+import PropTypes, { object } from "prop-types";
 import styled from "@emotion/styled";
 import Footer from "./components/footer/footer";
 import LoadingIntro from "./components/loading-intro/loading-intro";
@@ -8,6 +9,12 @@ import Header from "./containers/header/header";
 import { Global } from "@emotion/core";
 import { globalStyles } from "../styles";
 
+// if in dev mode
+if (process.env.NODE_ENV === 'development') {
+  // Set react-axe to catch accessibility issues in dev mode
+  const axe - require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
 
 const Main = styled.main`
   margin: 0 auto;
@@ -15,6 +22,23 @@ const Main = styled.main`
 
 const Layout = ({ children }) => {
   const [ loading, setLoading] = useState(true);
+
+  let AOS;
+  useEffect(() => {
+    // Server-side rendering is not provide the 'document' object,
+    // so this import is needed in either useEffect or componentDidMount,
+    // because are executed in client side
+    const AOS = require('aos');
+    AOS.init({
+      once: true,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (AOS) {
+      AOS.refresh();
+    }
+  });
 
   return (
     <React.Fragment>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { graphql } from "gatsby";
 import Layout from "./components/layout";
 import SEO from "./components/seo";
 import Hero from "../containers/hero/hero";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import About from "./containers/about/about";
 import Contact from "./containers/contact/contact";
 import Skills from "./components/skills/skills";
-
-// if in dev mode
-if (process.env.NODE_ENV === 'development') {
-  // Setup react-axe to catch accessibility errors during development
-  const axe = require('react-axe');
-  axe(React, ReactDOM, 1000);
-}
-
-AOS.init({
-  once: true,
-});
 
 const IndexPage = () => {
   return (
